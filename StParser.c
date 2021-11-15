9507d14c470be85f0e1211ef383dc0cbcea7c7f3
Benjamin Jack Johnston <50012332@serc.ac.uk>
2021-11-15 13:36:01 +0000

    Started work on parser

--- StParser.c
+++ StParser.c
@@ -2,5 +2,12 @@
 
 int StParse_Parse(StLex_Token* startToken)
 {
+    StLex_Token* head = startToken;
 
+    while (startToken != 0)
+    {
+        
+        // Goto next token
+        head = head->nextToken;
+    }
 }
--- StParser.h
+++ StParser.h
@@ -1,3 +1,14 @@
+#ifndef STPARSER_H
+#define STPARSER_H
+
 #include "StLex.h"
 
+struct StParser_Leaf
+{
+    
+};
+
+
 int StParse_Parse(StLex_Token* startToken);
+
+#endif
